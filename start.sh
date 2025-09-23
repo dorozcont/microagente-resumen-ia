e07924ca56d64442597c9871a3749dac633cbe41
dorozcont <dorozco@n.technology>
2025-09-23 09:45:04 -0600

    Test git-realease

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 echo "Iniciando el microagente de resumen de incidentes..."
 
-# Comando para iniciar la aplicación Python
+# Comando para iniciar la aplicación Python.
 python app.py
 
 echo "Microagente iniciado. ¡Accede a la interfaz web!"
